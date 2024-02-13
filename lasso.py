586a941511c125b43ae1586ce97042f6a2870f6b
mdaiyub <aiyubali15-13456@diu.edu.bd>
2024-02-13 11:35:02 +0600

    initial commit

---
+++ lasso.py
@@ -0,0 +1,82 @@
+import os
+import warnings
+import sys
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import StandardScaler
+from urllib.parse import urlparse
+import mlflow
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn
+
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    # Read the wine quality csv file from the URL
+    csv_url = (
+        "https://raw.githubusercontent.com/mlflow/mlflow/master/tests/datasets/winequality-red.csv"
+    )
+    try:
+        data = pd.read_csv(csv_url, sep=";")
+    except Exception as e:
+        logger.exception(
+            "Unable to download the dataset"
+        )
+    # Split the data into training set and test set (0.75, 0.25)
+    train, test = train_test_split(data)
+
+    # The prediction column is "quality"
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+    # Normalize features
+    scaler = StandardScaler()
+    train_x_scaled = scaler.fit_transform(train_x)
+    test_x_scaled = scaler.transform(test_x)
+
+    with mlflow.start_run():
+        lasso = Lasso(alpha=0.1)  # You can set the alpha value as needed
+        lasso.fit(train_x_scaled, train_y)
+
+        predicted_qualities = lasso.predict(test_x_scaled)
+
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        print("Lasso Regression model:")
+        print(" RMSE: %s" % rmse)
+        print(" MAE: %s" % mae)
+        print(" R2: %s" % r2)
+
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        predictions = lasso.predict(train_x_scaled)
+        signature = infer_signature(train_x_scaled, predictions)
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        # Model registry does not work with file store
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(
+                lasso, "model", registered_model_name="LassoRegressionWineModel", signature=signature
+            )
+        else:
+            mlflow.sklearn.log_model(lasso, "model", signature=signature)
